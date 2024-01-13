080794deea24a99ced73396d066bf668770db975
Yogendra kesharwani <121127@student.nitandhra.ac.in>
2024-01-13 19:15:58 +0530

    env file added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const http = require("http")
 const cors = require("cors")
-
+require("dotenv").config()
 app.use(cors())
 app.use(express.json())
 app.use(express.static(__dirname + '/public'))
@@ -28,7 +28,7 @@ const pro = async()=>{
 
 pro()
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 server.listen(PORT,()=>{
     console.log(`server connected to 3001`)
 })
